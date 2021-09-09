b0b6f974e0866116069db6fae5d3f79323616ffd
hapi190601 <gc1206ro@gmail.com>
2021-09-09 04:09:58 +0000

    Test2Fin

--- sample.java
+++ sample.java
@@ -1,7 +1,121 @@
 public class sample {
   public static void main(String args[]){
-    int[][] a = { {3, 5, 1}, {6, 2, 7}, {9, 4, 8} };
+    // 問１
+    // int[][] a = { {3, 5, 1}, {6, 2, 7}, {9, 4, 8} };
     
-    System.out.println(a[1][2]);
+    // System.out.println(a[1][2]);
+    
+    // 問２
+    // long a = 132642L;
+    // long b = 0L;
+    // b = a;
+    // System.out.println(b);
+    
+    // 問３
+    // int[][][] a = {{{},{1}},{{},{2}}};
+    // int[][][] a = new int[2][2][2];
+    // ↓誤り
+    // int[][][] a = new int[2][2][2]{{{},{1}},{{},{2}}};
+    // int[][][] a = new int[][][]{{{},{1}},{{},{2}}};
+
+    // 問４
+    // int[] a = {16, 9, 7, 2, 13};
+    // int b = a[2];
+    // a = new int[]{2, 15, 8, 4};
+    // int c = a[3];
+    // int d = b * c;
+    // System.out.println(d);
+    
+    // // 問５
+    // float f1 = 3.14F;
+    // double d2 = 1.4141592D;
+    // // ↓誤り
+    // float f3 = d2;
+    // double d4 = f1;
+    
+    // 問６
+    // float f1 = 10F / 3F;
+    // System.out.println(f1);
+    
+    // 問７
+    // ↓誤り
+    // float f1 = 3.14;
+    // double d2 = 1.4141592D;
+    // float f3 = 114514.11F;
+    // double d4 = 1112d;
+    
+    // 問９
+    // int a = 4;
+    // int b = 9;
+    // int c = 6;
+    // b = c;
+    // a = b;
+    // c = a;
+    // System.out.println(c);
+    
+    // 問１１
+    // int[][][] a = new int[2][4][3];
+    // for(int i = 0;i < a.length; i++){
+    //     for(int j = 0; j < a[i].length; j++){
+    //         for(int k = 0; k < a[i][j].length; k++){
+    //             a[i][j][k] = (i + j + k) * 3;
+    //             System.out.print(a[i][j][k] + "\t");
+    //         }
+    //         System.out.println();
+    //     }
+    //     System.out.println();
+    // }
+    
+    // 問１２
+    // エラーになる
+    // byte a = 120;
+    // a = 130;
+    // System.out.println(a);
+    
+    // 問１３
+    // int a = 5;
+    // int b = a - 3;
+    // System.out.println(b);
+    
+    // // 問１４
+    // boolean bl1 = true;
+    // boolean bl2 = false;
+    // bl2 = bl1;
+    // if(bl2){
+    //     System.out.println("TRUE");
+    // } else {
+    //     System.out.println("FALSE");
+    // }
+
+    // 問１８
+    // byte[] a = {1,4,7};
+    // short[] b = new short[5];
+    // int c = a.length * b.length;
+    // System.out.println(c);
+
+  
+    // 問１９
+    // char p = 'a';
+    // char q = 'い';
+    // char r = 'C';
+  
+    // p = r;
+    // q = p;
+    // r = q;
+  
+    // System.out.print(q);
+    // System.out.print(r);
+    // System.out.print(p);
+    
+    // 問２０
+    // double d1 = 13;
+    // double d2 = 4;
+    // double d3 = d1 /d2;
+    // System.out.println(d3);
+    
+    
+    
+    
+    
   }
 }
